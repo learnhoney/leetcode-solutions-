e0b83b180371afe9c60ccd1454ce12ff957ce078
honey <asmigoyal11@gmail.com>
2024-09-12 23:56:22 +0530

    Time: 48 ms (18.93%), Space: 38.6 MB (19.26%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int k) {
-        return counterfun(nums,k)-counterfun(nums, k-1);
-    }
-
-    int counterfun(vector<int>& nums, int k){
-        if (k<0){
-            return 0; 
-        }
-        int r=0, l=0;
-        long long sum=0;
-        int count = 0;
-        int n=nums.size();
-        while(r<n){
-            sum+=nums[r];
-            while(sum>k){
-                sum-=nums[l];
-                l++;
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> map;
+        int count=0, sum=0;
+        map[0]=1;
+        for(int i=0; i<nums.size();i++){
+            sum+=nums[i];
+            if(map.find(sum-goal)!=map.end()){
+                count+=map[sum-goal];
             }
-            count+=(r-l+1);
-            r++;
+            map[sum]++;
         }
         return count;
     }
-    
 };
