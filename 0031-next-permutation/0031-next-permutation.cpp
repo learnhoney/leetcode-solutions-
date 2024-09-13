5686d3f6e9367fc644b945073c0d762ccc5b7fe2
honey <asmigoyal11@gmail.com>
2024-09-14 00:49:55 +0530

    Time: 0 ms (100%), Space: 14.9 MB (53.69%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int dip=-1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                dip=i;
+                break;
+            }
+        }
+        if(dip==-1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i=n-1; i>dip; i--){
+            if(nums[i]>nums[dip]){
+                swap(nums[i],nums[dip]);
+                break;
+            }
+        }
+        reverse(nums.begin()+dip+1, nums.end());
+        
+    }   
+};
