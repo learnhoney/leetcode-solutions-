7cfd1d81e95e88425e81e37cf36773d3cb50e0cb
honey <asmigoyal11@gmail.com>
2024-09-19 21:35:25 +0530

    Time: 12 ms (93.85%), Space: 24.8 MB (84.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,9 +5,9 @@ public:
 
         if (n==1) return nums[0];
         if (nums[0]!=nums[1]) return nums[0];
-        //if (nums[n-1]!=nums[n-2]) return nums[n-1];
+        if (nums[n-1]!=nums[n-2]) return nums[n-1];
 
-        int low=1, high=n-1;
+        int low=1, high=n-2;
         while (low<=high) {
             int mid=(low+high)/2;
 
