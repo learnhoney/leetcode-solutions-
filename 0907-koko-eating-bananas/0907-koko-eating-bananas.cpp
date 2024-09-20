c719152943efb15b4c6500a18cc0d938f80b983a
honey <asmigoyal11@gmail.com>
2024-09-20 13:01:55 +0530

    Time: 33 ms (47.23%), Space: 21.6 MB (81.53%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long rate(vector<int>& piles, int hoursatmid){
+        long long tothrs=0;
+        for(double i: piles){
+            tothrs+=ceil(i/hoursatmid);
+        }
+        return tothrs;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int maxi= *max_element(piles.begin(), piles.end());
+        int low=1;
+        int high=maxi;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(rate(piles, mid)<=h){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
