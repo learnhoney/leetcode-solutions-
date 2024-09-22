13c0da731cf8d70db52b75d8aeb7a10731c9ceda
honey <asmigoyal11@gmail.com>
2024-09-22 16:06:27 +0530

    Time: 16 ms (90.74%), Space: 94.7 MB (87.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        int i=0, j=0; 
-        double median;
-        vector<int> res;
-        while(i<n && j<m){
-            if(nums1[i]<nums2[j]){
-                res.push_back(nums1[i]);
-                i++;
-            }else{
-                res.push_back(nums2[j]);
-                j++;
-                }
-        }
-        while (i < n) {
-            res.push_back(nums1[i]);
-            i++;
-        }
-        while (j < m) {
-            res.push_back(nums2[j]);
-            j++;
-        }
-        
-        if(res.size()%2==0){
-            median=(res[res.size()/2 -1] +res[res.size()/2 ])/2.0;
-        }else{
-            median=res[res.size()/2];
+        int n1=nums1.size(), n2=nums2.size();
+        int mn=n1+n2;
+        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
+        int formula=(n1+n2+1)/2;
+
+        int low=0, high=n1;
+        while(low<=high){
+            int mid1=(low+high) >> 1;
+            int mid2=formula-mid1;    //symmetry left righting
+
+            int l1=INT_MIN, l2=INT_MIN;
+            int r1=INT_MAX, r2=INT_MAX; //initialisations r,l
+            if(mid1<n1){
+                r1=nums1[mid1];
             }
-        return median;
+            if(mid2<n2){
+                r2=nums2[mid2];
+            }
+            if(mid1-1>=0){
+                l1= nums1[mid1-1];
+            }
+            if(mid2-1>=0){
+                l2=nums2[mid2-1];
+            }
+            //cndxns 
+            if(l1<=r2 && l2<=r1){
+                if(mn%2==1){
+                    return max(l1,l2);
+                }else{
+                    return ((double)(max(l1,l2)+min(r1,r2)))/2.0;
+                }
+            }else if(l1>r2){
+                high=mid1-1;
+            }else{
+                low=mid1+1;
+            }
+        }
+
+        return 0;
     }
-    
 };
