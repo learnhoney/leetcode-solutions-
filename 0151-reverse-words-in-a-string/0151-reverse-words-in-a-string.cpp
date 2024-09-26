db7875b81e7a5985e5c62077cefa7e0d6f8d30f1
honey <asmigoyal11@gmail.com>
2024-09-26 15:30:18 +0530

    Time: 6 ms (46.21%), Space: 8.8 MB (73.32%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res, helper;
+        int i=s.length()-1;
+        string ans;
+        for(int j=i; j>=0;j--){
+            if(s[j]==' '){
+                if(helper.size()!=0){
+                    reverse(helper.begin(), helper.end());
+                    ans+=helper;
+                    helper="";
+                    ans+=" ";
+                }
+            }else{
+                helper+=s[j];
+            }
+        }
+        if(helper.size()!=0){
+            reverse(helper.begin(),helper.end());
+            ans+=helper;
+        }else if(ans.size()!=0) ans.pop_back();
+        return ans;
+    }
+};
