c071569b24672d597651ea197df1933ea1f75278
honey <asmigoyal11@gmail.com>
2024-10-06 17:47:16 +0530

    Time: 2 ms (43.7%), Space: 8.4 MB (75.83%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+        
+        for(int i=1; i < s.length(); i++){
+            sum += abs((s[i] - 'a') - (s[i - 1] - 'a'));
+        }
+       return sum; 
+    }
+};
