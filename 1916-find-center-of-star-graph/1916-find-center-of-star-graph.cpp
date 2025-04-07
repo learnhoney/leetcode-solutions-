c50e3ecea4185e75cee9673342c6f5059c691ba4
honey <asmigoyal11@gmail.com>
2025-04-07 20:56:42 +0530

    Time: 0 ms (100%), Space: 99.2 MB (39.22%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size();
-        unordered_map<int,int> hm;
-        for(auto it : edges){
-            hm[it[0]]++;
-            hm[it[1]]++;   
-            }
-
-        for(const auto& it : hm){
-            if(it.second == n){
-                return it.first;
-            }
+        pair p ={ edges[0][0], edges[0][1] };
+        if(edges[1][0] == p.first || edges[1][1] == p.first){
+            return p.first;
         }
-        return -1;
+        return p.second;
     }
-};
+}; 
